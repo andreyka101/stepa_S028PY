6274b564d6e5b349dec7cc7857e2595c713ea7c7
andreyka101 <andrejkvasnin236@gmail.com>
2025-11-12 20:04:34 +0300

    кпккпкпкп

--- lesson_1.py
+++ lesson_1.py
@@ -1,14 +1,22 @@
+# импортируем библиотеку tkinter
 from tkinter import *
 
 
+# создаём окно 
 window = Tk()
-window.title("lesson")
-window.geometry("700x500")
+# меняем название окна
+window.title("lesson 11")
+# меняем ширину и высоту окна 
+window.geometry("600x500")
 
 
 
-
+# создаём текст через Label
+# bg= цвет фона 
+# fg= цвет текстам
+# font = шрифт и размер текста 
 lab_text1 = Label(text="555444" , bg="#569dcb") 
+# placе - позиция и размер элемента
 lab_text1.place(x=300, y=400 , width= 100 , height=50)
 
 lab_text2 = Label(text="text2" , bg="#310505", fg="#f6ff47" , font=("Algerian" , 18)) 
@@ -22,8 +30,14 @@ lab_text2.place(x=300, y=200)
 def fun_1():
     print("click")
     window.title("text")
+
+    # изменяем позицию lab_text_1
     lab_text2.place(x=400, y=300)
+
+    # # меняем ширину и высоту окна
     # window.geometry("500x300")
+
+    # с помощью метода configure или config можно изменить параметры объекта 
     but1.config(bg="#53ff44",font=("Algerian" , 23))
     lab_text2.configure(text="click button" ,font=("Algerian" , 23) , bg="#5bfaff")
 
@@ -42,3 +56,11 @@ but1.place(x=100, y=400)
 
 window.mainloop()
 print("end")
+
+
+
+# задача 1
+# Создайте игру clicker, при нажатии на кнопку должно число увеличиваться на один
+
+# задача 2
+# Сделать игру найди кнопку, при нажатии на кнопку она перемещается в случайное место по координатам ,но не выходит за границы окна
